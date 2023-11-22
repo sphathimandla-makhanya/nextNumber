d722c6fea497660ee4c9c0b02c00a787a7a4d939
sphathimandla-makhanya <makhanyasm11@gmail.com>
2023-11-22 15:42:42 +0200

    created files

---
+++ javascript/index.js
@@ -0,0 +1,18 @@
+let numbersArray = [5, 3, 8, 6, 7, 10, 2]
+let v=0;
+
+function numbers() {
+    if (v<numbersArray.length) {
+        let nextValue = numbersArray[v];
+        v++
+        console.log(nextValue);
+        view.value=nextValue
+    } 
+    else{
+        v = 0;
+    }
+}
+
+let btn= document.querySelector('[data-btn]')
+btn.addEventListener('click', numbers)
+let view= document.querySelector('[data-view]')
